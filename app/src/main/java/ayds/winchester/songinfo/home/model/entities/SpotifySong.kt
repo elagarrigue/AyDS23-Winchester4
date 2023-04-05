397682813d8e77db72e126cec37fdfa1fb86f3fd
joachimpod <42682629+joachimpod@users.noreply.github.com>
2023-04-05 10:17:49 -0300

    Revert "Cambios en la clase Song."

--- app/src/main/java/ayds/winchester/songinfo/home/model/entities/SpotifySong.kt
+++ app/src/main/java/ayds/winchester/songinfo/home/model/entities/SpotifySong.kt
@@ -8,10 +8,13 @@ sealed class Song {
         val artistName: String,
         val albumName: String,
         val releaseDate: String,
-        val releaseDatePrecision: String,
         val spotifyUrl: String,
         val imageUrl: String,
         var isLocallyStored: Boolean = false
-    ) : Song()
+    ) : Song() {
+
+        val year: String = releaseDate.split("-").first()
+    }
+
     object EmptySong : Song()
 }
