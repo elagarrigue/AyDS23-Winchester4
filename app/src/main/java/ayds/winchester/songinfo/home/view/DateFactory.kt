dc60f30ef892d99161823dd77cd63d74ce483437
Brun0Mand0lesi <89532441+Brun0Mand0lesi@users.noreply.github.com>
2023-04-17 09:40:01 -0300

    se corrigieron nombre
    se consulto por la preferencia de los nombres y de las constantes y se decidió cambiar dia, mes y anio a day, month y year

--- app/src/main/java/ayds/winchester/songinfo/home/view/DateFactory.kt
+++ app/src/main/java/ayds/winchester/songinfo/home/view/DateFactory.kt
@@ -7,18 +7,18 @@ import DateConverterMonthImpl
 import DateConverterYearImpl
 import ayds.winchester.songinfo.home.model.entities.Song
 
-private const val dia ="day"
-private const val mes="month"
-private const val anio="year"
+private const val day ="day"
+private const val month="month"
+private const val year="year"
 interface DateFactory {
     fun getDateConverted( song: Song.SpotifySong):Converter
 }
 class DateFactoryImpl:DateFactory{
     override fun getDateConverted( song: Song.SpotifySong) =
         when (song.releaseDatePrecision) {
-            dia -> DateConverterDayImpl(song.releaseDate)
-            mes -> DateConverterMonthImpl(song.releaseDate)
-            anio -> DateConverterYearImpl(song.releaseDate)
+            day -> DateConverterDayImpl(song.releaseDate)
+            month -> DateConverterMonthImpl(song.releaseDate)
+            year -> DateConverterYearImpl(song.releaseDate)
             else -> DateConverterDefaultImpl(song.releaseDate)
         }
     }
