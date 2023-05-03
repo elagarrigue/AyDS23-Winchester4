379760f247ac7e4ca4ba872d423b3e67afa5c981
joachimpod <joachimpodlesch@hotmail.com>
2023-05-03 10:46:07 -0300

    Modificaciones en funciones
    se hicieron los arreglos que fueron solicitados

--- app/src/main/java/ayds/winchester/songinfo/moredetails/fulllogic/InfoAboutArtist.kt
+++
@@ -1,8 +0,0 @@
-package ayds.winchester.songinfo.moredetails.fulllogic
-
-data class InfoAboutArtist(
-    var artistName: String?,
-    var generalInformation: String?,
-    var url: String?,
-    var existInDatabase: Boolean) {
-}
