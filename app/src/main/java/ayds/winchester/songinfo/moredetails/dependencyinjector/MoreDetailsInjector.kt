ee943127768ef6e9ac10b929f0b9725fb95e5221
joachimpod <joachimpodlesch@hotmail.com>
2023-05-10 18:35:15 -0300

    Created Injector
    Se fucionaron las clases inyector en una sola clase

---
+++ app/src/main/java/ayds/winchester/songinfo/moredetails/dependencyinjector/MoreDetailsInjector.kt
@@ -0,0 +1,60 @@
+package ayds.winchester.songinfo.moredetails.dependencyinjector
+
+import android.content.Context
+import ayds.winchester.songinfo.moredetails.data.external.WikipediaService
+import ayds.winchester.songinfo.moredetails.data.external.wikipedia.JsonToArtistResolver
+import ayds.winchester.songinfo.moredetails.data.external.wikipedia.WikipediaAPI
+import ayds.winchester.songinfo.moredetails.data.external.wikipedia.WikipediaServiceImpl
+import ayds.winchester.songinfo.moredetails.data.external.wikipedia.WikipediaToArtistResolver
+import ayds.winchester.songinfo.moredetails.data.local.ArtistLocalStorage
+import ayds.winchester.songinfo.moredetails.data.local.sqldb.ArtistLocalStorageImpl
+import ayds.winchester.songinfo.moredetails.data.local.sqldb.CursorToArtistMapperImpl
+import ayds.winchester.songinfo.moredetails.data.repository.ArtistRepositoryImpl
+import ayds.winchester.songinfo.moredetails.domain.repository.ArtistRepository
+import ayds.winchester.songinfo.moredetails.presentation.presenter.MoreDetailsPresenter
+import ayds.winchester.songinfo.moredetails.presentation.presenter.MoreDetailsPresenterImpl
+import ayds.winchester.songinfo.moredetails.presentation.view.ArtistDescriptionHelper
+import ayds.winchester.songinfo.moredetails.presentation.view.ArtistDescriptionHelperImpl
+import ayds.winchester.songinfo.moredetails.presentation.view.MoreDetailsUiState
+import ayds.winchester.songinfo.moredetails.presentation.view.MoreDetailsView
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+private const val BASE_URL = "https://en.wikipedia.org/w/"
+
+object MoreDetailsInjector {
+
+    private val wikipediaAPIRetrofit = Retrofit.Builder()
+                                               .baseUrl(BASE_URL)
+                                               .addConverterFactory(ScalarsConverterFactory.create())
+                                               .build()
+    private val wikipediaAPI = wikipediaAPIRetrofit.create(WikipediaAPI::class.java)
+    private val wikipediaToArtistResolver: WikipediaToArtistResolver = JsonToArtistResolver()
+
+    private val wikipediaService: WikipediaService =  WikipediaServiceImpl(
+        wikipediaAPI,
+        wikipediaToArtistResolver
+    )
+
+    val artistDescriptionHelper: ArtistDescriptionHelper = ArtistDescriptionHelperImpl()
+
+    private lateinit var  moreDetailsPresenter: MoreDetailsPresenter
+
+    fun getMoreDetailsPresenter(): MoreDetailsPresenter = moreDetailsPresenter
+
+    fun initOnViewStarted(moreDetailsView: MoreDetailsView) {
+
+        val artistLocalStorage: ArtistLocalStorage = ArtistLocalStorageImpl(
+            moreDetailsView as Context, CursorToArtistMapperImpl()
+        )
+        val wikipediaService: WikipediaService = wikipediaService
+
+        val repository: ArtistRepository =
+            ArtistRepositoryImpl(artistLocalStorage, wikipediaService)
+
+        moreDetailsPresenter = MoreDetailsPresenterImpl(repository, MoreDetailsUiState()
+        )
+    }
+
+
+}
--- app/src/main/java/ayds/winchester/songinfo/moredetails/dependencyinjector/WikipediaInjector.kt
+++
@@ -1,8 +0,0 @@
-package ayds.winchester.songinfo.moredetails.dependencyinjector
-
-import ayds.winchester.songinfo.moredetails.data.external.WikipediaService
-
-object WikipediaInjector {
-
-    val wikipediaService: WikipediaService = WikipediaQueryInjector.wikipediaService
-}
