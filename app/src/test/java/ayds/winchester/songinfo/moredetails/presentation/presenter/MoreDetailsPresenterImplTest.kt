35c2613a98cf6b9474889b23a303fb000b8aab7d
Iara <iaravera3333@gmail.com>
2023-05-15 18:16:58 -0300

    Se empezo a implementar el test de MoreDetailsPresenter

--- app/src/test/java/ayds/winchester/songinfo/moredetails/presentation/presenter/MoreDetailsPresenterImplTest.kt
+++ app/src/test/java/ayds/winchester/songinfo/moredetails/presentation/presenter/MoreDetailsPresenterImplTest.kt
@@ -1,6 +1,40 @@
 package ayds.winchester.songinfo.moredetails.presentation.presenter
 
 
-internal class MoreDetailsPresenterImplTest{
+import ayds.winchester.songinfo.moredetails.domain.entities.Artist
+import ayds.winchester.songinfo.moredetails.domain.repository.ArtistRepository
+import ayds.winchester.songinfo.moredetails.presentation.MoreDetailsUiState
+import io.mockk.*
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
 
+internal class MoreDetailsPresenterImplTest {
+
+    private lateinit var presenter: MoreDetailsPresenterImpl
+    private val repository: ArtistRepository = mockk()
+    private val artistDescriptionHelper: ArtistDescriptionHelper = mockk()
+
+    @Before
+    fun setUp() {
+        presenter = MoreDetailsPresenterImpl(repository, artistDescriptionHelper)
+    }
+
+    @Test
+    fun searchArtist_setsCorrectUiState() {
+        // Arrange
+        val artistName = "Artist Name"
+        val artist = Artist.ArtistInfo("Artist Name", "Artist Description", "Artist Wikipedia URL")
+        val expectedUiState = MoreDetailsUiState("Artist Image url","Artist Description", "Artist Wikipedia URL")
+        coEvery { repository.getArtistByName(artistName) } returns artist
+        every { artistDescriptionHelper.getArtistDescriptionText(artist) } returns "Artist Description"
+
+        // Act
+        presenter.searchArtist(artistName)
+
+        // Assert
+        val capturedState = slot<MoreDetailsUiState>()
+        verify { presenter.artistObservable.notify(capture(capturedState)) }
+        assertEquals(expectedUiState, capturedState.captured)
+    }
 }
